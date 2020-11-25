0412d301677bbd396611ae9a8088d70155113414
권나영 <u201035@may.ulsan.ac.kr>
2020-11-25 16:01:48 +0900

    최종적으로 다듬기

--- project.py
+++ project.py
@@ -1,9 +1,7 @@
-# comment 추가하세요
 
-numbers = range(100) #윗단계에서 받아낸 숫자를 primes라는 리스트에 담아두자.
+numbers = range(100) #윗단계에서 받아낸 숫자를 numbers라는 리스트에 담아두자.
 is_primes=[] #소수를 담을 리스트
 isnt_primes=[] #소수가 아닌 수를 담을 리스트
-#여기서 받아온 primes 리스트를 아래 식에 하나씩 대입하는 방법을 잘 모르겠네요.
 
 for n in numbers:
     if n<=1:
